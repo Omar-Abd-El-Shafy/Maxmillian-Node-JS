3227653d25cf34b4d6be21bae6f2b3e150dffb73
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-06-28 16:54:55 +0200

    changed app.js location ,redirect and routiong

---
+++ ch3/app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("content-type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"></input> <button type="submit">Send</button></form>'
+    );
+
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("content-type", "text/html");
+  res.write("<html>");
+  res.write("<body>");
+  res.write("<h1> HELLO </h1>"
+  );
+
+  res.write("</body>");
+  res.write("</html>");
+  return res.end();
+});
+
+server.listen(3000);
